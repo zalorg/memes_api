8f9f2aa3613c1ae372dcf61ef08d8f81274ef59e
Zalán Horváth <horvathzalan@zal1000.com>
2021-08-24 02:32:02 +0000

    Yup (from codespasces)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import { initializeApp } from 'firebase-admin'
+import { initializeApp, credential } from 'firebase-admin'
 import * as express from 'express'
 import router from './router'
 import fetchSubreddits from './functions/fetchSubreddits'
 
+
 initializeApp({
+    credential: credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!)),
     databaseURL: 'https://memes-api-cache.europe-west1.firebasedatabase.app/'
 })
 
 const app = express();
+
 
 app.use(router);
 
---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express'
+
+export default function (req: Request, res: Response) {
+  res.sendStatus(200);
+}
+export const autoRegister = true
