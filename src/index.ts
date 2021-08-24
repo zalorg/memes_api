96be40c19b1cfeae207c508f093de54e3d1b8a6e
Zalán Horváth <horvathzalan@zal1000.com>
2021-08-24 19:49:48 +0000

    Yup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { initializeApp, credential } from 'firebase-admin'
+import { initializeApp } from 'firebase-admin'
 import * as express from 'express'
 import router from './router'
 // import fetchSubreddits from './functions/fetchSubreddits'
 
 initializeApp({
-    credential: credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!)),
+    // credential: credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!)),
     databaseURL: 'https://memes-api-cache.europe-west1.firebasedatabase.app/'
 })
 
@@ -13,7 +13,6 @@ const app = express();
 app.use(express.json())
 app.use(router);
 
-// console.log(process.env.PERSONAL_DISCORD_BOT_TOKEN)
 // fetchSubreddits('memes')
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
